5ffabeda2182e821512d6380e65bb77e5a2f8304
Yimoy2 <l.h.ying1997@gmail.com>
2025-01-04 23:25:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+const buttons = document.querySelectorAll('.sound-btn');
+const audio = document.getElementById('audio');
 
-sounds.forEach(sound => {
-    const btn = document.createElement('button');
-    btn.classList.add('btn');
-    btn.innerText = sound;
-
-    btn.addEventListener('click', () => {
-        stopSounds();
-
-        document.getElementById(sound).play();
-    })
-
-    document.querySelector('#buttons').appendChild(btn);
-})
-
-function stopSounds() {
-    sounds.forEach(sound => {
-        const audio = document.getElementById(sound);
-        audio.pause();
-        audio.currentTime = 0;
-    })
-}
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const sound = button.getAttribute('data-sound');
+    audio.src = `sounds/${sound}.mp3`; // Make sure your sound files are in a "sounds" folder
+    audio.play();
+  });
+});
